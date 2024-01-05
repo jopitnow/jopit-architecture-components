cbe3999755c1271117f5cec75cd687d5cccc37e8
Guido Simón Perre <perreguido@gmail.com>
2024-01-05 19:30:55 -0300

    Adding publish action

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,51 +1,5 @@
-pluginManagement {
-    repositories {
-        google()
-        gradlePluginPortal()
-        mavenCentral()
-        mavenLocal()
-        maven {
-            url = uri("http://mobile.jopit.com.ar:8081/repository/maven-releases/")
-            isAllowInsecureProtocol = true
-            credentials {
-                username = System.getenv("NEXUS_JOPIT_USER")
-                password = System.getenv("NEXUS_JOPIT_PASSWORD")
-            }
-        }
-    }
-}
-
-@Suppress("UnstableApiUsage")
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        mavenLocal()
-        maven {
-            url = uri("http://mobile.jopit.com.ar:8081/repository/maven-releases/")
-            isAllowInsecureProtocol = true
-            credentials {
-                username = System.getenv("NEXUS_JOPIT_USER")
-                password = System.getenv("NEXUS_JOPIT_PASSWORD")
-            }
-        }
-        maven {
-            url = uri("http://mobile.jopit.com.ar:8081/repository/maven-experimental/")
-            isAllowInsecureProtocol = true
-            credentials {
-                username = System.getenv("NEXUS_JOPIT_USER")
-                password = System.getenv("NEXUS_JOPIT_PASSWORD")
-            }
-        }
-        versionCatalogs {
-            create("jopitCatalog") {
-                from("com.gperre.jopit:jopit-version-catalog:+")
-            }
-        }
-    }
-}
+apply(from = "https://dependencies.jopit.com.ar/settings.gradle.kts")
 
 rootProject.name = "jopit-architecture-components"
+
 include(":architecture-components-android")
-include(":architecture-components-shared")
